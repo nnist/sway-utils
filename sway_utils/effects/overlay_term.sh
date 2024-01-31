4f381094d092a2b4bdab0f2de4a1a4ed1aac01da
Nicole Nisters <n.nisters@pm.me>
2024-01-31 16:10:19 +0100

    Fix overlay term effect

--- sway_utils/effects/overlay_term.sh
+++ sway_utils/effects/overlay_term.sh
@@ -2,9 +2,6 @@ alacritty \
     --option font.size=24 \
     window.dimensions.columns=40 \
     window.dimensions.lines=7 \
-    colors.cursor.cursor=\"CellBackground\" \
-    colors.cursor.text=\"CellBackground\" \
-    cursor.style=\"Underline\" \
-    background_opacity=0.0 \
+    window.opacity=0.0 \
     --class=overlay \
     -e /bin/bash -c "source $HOME/.config/base16-shell/scripts/base16-default-dark.sh; tput civis; $1"
